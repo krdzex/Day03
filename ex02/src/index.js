5197faeb883ad1919a6839b25505c934fef75023
Krsto Kostic <kostic.krsto@gmail.com>
2021-03-10 22:33:30 +0100

    Day 03

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // Ony change code below this line
+// Single line comment
+/* Multi multi
+line line
+comment comm
+*/
+    // Only change code above this line
+}
+console.log(main());
+module.exports=main;
